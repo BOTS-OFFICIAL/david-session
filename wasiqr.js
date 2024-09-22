4033b10323693f874f3d483af70d50760e24e7f7
BOTS-OFFICIAL <botsofficial90@gmail.com>
2024-09-22 19:00:49 +0200

    Update wasiqr.js

--- wasiqr.js
+++ wasiqr.js
@@ -60,19 +60,20 @@ router.get('/', async (req, res) => {
 	
 				   let WASI_MD_TEXT = `
 ┏━━━━━━━━━━━━━━
-┃QUEEN_ANITA_V2 SESSION IS 
+┃KENTON X BOTS SESSION IS 
 ┃SUCCESSFULLY
 ┃CONNECTED ✅🔥
 ┗━━━━━━━━━━━━━━━
 ▬▬▬▬▬▬▬▬▬▬▬▬▬▬
-❶ || Creator = 𖥘 DAVID CYRIL 𖥘
+❶ || Creator = 𖥘 MAGICAL-KX 𖥘
 ▬▬▬▬▬▬▬▬▬▬▬▬▬▬
-❷ || WhattsApp Channel = https://whatsapp.com/channel/0029VaeRru3ADTOEKPCPom0L
+❷ || https://whatsapp.com/channel/0029Vah4I4P2UPB8vOAmDV3V
 ▬▬▬▬▬▬▬▬▬▬▬▬▬▬
 Please Follow My Support Channel
-Wanna talk to me?👉 https://t.me/deecee_x 👈
+Wanna talk to me?👉 https://t.me/Kentonx 👈
+For Whatsapp get in touch : https://wa.me/263789432940
 ▬▬▬▬▬▬▬▬▬▬▬▬▬▬
-©*2024-2099 David Cyril*
+©*2024-2099 BOTS OFFICIAL*
 _____________________________________
 	
 _Don't Forget To Give Star To My Repo_`
